6863839ca13845d72575d95c30d0506ae9a682b4
Jason Parrott <fanatic+github@gmail.com>
2018-12-18 11:15:50 -0500

    Update imports

--- examples/client.go
+++ examples/client.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"github.com/graarh/golang-socketio"
-	"github.com/graarh/golang-socketio/transport"
+	"github.com/fanatic/golang-socketio"
+	"github.com/fanatic/golang-socketio/transport"
 	"log"
 	"runtime"
 	"time"
